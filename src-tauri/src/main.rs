04a5b71ca7dc78112d540faf62a4fdbe790d2299
Lurian <lurian-code@protonmail.com>
2022-11-03 13:43:20 +0100

    Finished configuring project

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,20 +3,12 @@
     windows_subsystem = "windows"
 )]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    println!("Arguments of greet: {}", name);
-    let msg = format!("Hello, {}! You've been greeted from Rust!", name);
-    println!("Greet has been called! Name has been modified!");
-    return msg;
-}
 
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
-            greet
+
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,6 @@
 <script lang="ts">
-  import Greet from './components/Greet.svelte'
 </script>
 
-<main class="animate-bounce">
-  <h1>Welcome to Tauri!</h1>
-
-  <div>
-    <!-- svelte-ignore security-anchor-rel-noreferrer -->
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="animate-spin" alt="Vite Logo" />
-    </a>
-    <!-- svelte-ignore security-anchor-rel-noreferrer -->
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="animate-spin" alt="Tauri Logo" />
-    </a>
-    <!-- svelte-ignore security-anchor-rel-noreferrer -->
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="animate-spin" alt="Svelte Logo" />
-    </a>
-  </div>
-
-  <p>
-    Click on the Tauri, Vite, and Svelte logos to learn more.
-  </p>
-
-  <div>
-    <Greet header=345/>
-  </div>
-
+<main class="">
+  <h1>Welcome to Ludownloader</h1>
 </main>
