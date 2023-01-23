0cb16df7b194fd93ea44dea43336129dfe4bdc7d
lurian <lurian-code@protonmail.com>
2023-01-23 20:44:32 +0100

    added concurrent download test & stuff

--- downloader/src/httpdownload.rs
+++ downloader/src/httpdownload.rs
@@ -40,7 +40,7 @@ pub struct HttpDownload {
      */
     content_length: u64,
     /**
-     Amount of bytes downloaded, aka size of the file found at file_path.
+     Amount of bytes downloaded, same as size of the file found at file_path.
     * This value is calculated in case the download is started with resume() and is then updated for every downloaded chunk.
      */
     downloaded_bytes: u64,
@@ -52,6 +52,15 @@ pub struct HttpDownload {
 }
 
 impl HttpDownload {
+    /** Sends a request to the download server using encapsulated configuration and URL */
+    pub async fn get(&self) -> Result<reqwest::Response, reqwest::Error> {
+        self.client
+            .get(self.url.as_ref())
+            .timeout(self.config.timeout)
+            .headers(self.config.headers.clone())
+            .send()
+            .await
+    }
     /** Initializes a new HttpDownload.
      *  file_path: Path to the file, doesn't matter if it exists already.
      *  config: optional HttpDownloadConfig (to configure timeout, headers, retries, etc...)
@@ -59,6 +68,7 @@ impl HttpDownload {
     pub async fn new(
         url: Url,
         file_path: PathBuf,
+        client: reqwest::Client,
         config: Option<HttpDownloadConfig>,
     ) -> Result<Self, String> {
         // If no configuration is passed the default one is copied
@@ -69,8 +79,8 @@ impl HttpDownload {
             file_path,
             config,
             downloaded_bytes,
+            client,
             supports_byte_ranges: false,
-            client: Client::new(),
             content_length: 0u64,
         };
         download.update_server_data().await?;
@@ -81,47 +91,33 @@ impl HttpDownload {
      * Starts the Download from scratch
      */
     pub async fn start(&mut self) -> Result<(), String> {
-        // Send the friggin request
+        // Send the frigging request
         let resp = self
-            .client
-            .get(self.url.as_ref())
-            .timeout(self.config.timeout)
-            .headers(self.config.headers.clone())
-            .send();
+            .get()
+            .await
+            .map_err(|_| format!("Failed to send GET to: '{}'", self.url.as_str()))?;
         // Open the file
-        let mut file_handler = File::create(&self.file_path).await.map_err(|_| {
+        let mut file_handler = File::create(&self.file_path).await.map_err(|e| {
             format!(
-                "Failed creating/opening File for HttpDownload. path: {:?}",
-                self.file_path
+                "Failed creating/opening File for HttpDownload, path: {:?}, error: {:?}",
+                self.file_path, e
             )
         })?;
         // Await the response, raise error with String msg otherwise
-        let resp = resp
-            .await
-            .map_err(|_| format!("Failed to send GET to: '{}'", self.url.as_str()))?;
-
         if let Some(chunk_size) = self.config.chunk_size {
             // Chunked-Buffer download
             let mut stream = resp.bytes_stream().chunks(chunk_size);
             while let Some(buffered_chunks) = stream.next().await {
-                for result in buffered_chunks {
-                    match result {
-                        Ok(chunk) => {
-                            self.downloaded_bytes +=
-                                file_handler.write(&chunk).await.map_err(|e| {
-                                    format!(
-                                        "Error while writing to file at {:?}. Error: {:#?}",
-                                        self.file_path, e
-                                    )
-                                })? as u64
-                        }
-                        Err(e) => {
-                            return Err(format!(
-                                "Error while chunking download response. Error: {:?}",
-                                e
-                            ));
-                        }
-                    }
+                for item in buffered_chunks {
+                    let chunk = item.map_err(|e| {
+                        format!("Error while chunking download response. Error: {:?}", e)
+                    })?;
+                    self.downloaded_bytes += file_handler.write(&chunk).await.map_err(|e| {
+                        format!(
+                            "Error while writing to file at {:?}. Error: {:#?}",
+                            self.file_path, e
+                        )
+                    })? as u64;
                 }
             }
         } else {
@@ -151,19 +147,13 @@ impl HttpDownload {
     * This function doesn't fail or return errors, on worst case nothing happens
      */
     async fn update_server_data(&mut self) -> Result<(), String> {
-        let response = self
-            .client
-            .get(self.url.as_ref())
-            .timeout(self.config.timeout)
-            .send()
-            .await
-            .map_err(|err| {
-                format!(
-                    "Couldn't execute Head request! url: {:?}, error: {:#?}",
-                    self.url.as_str(),
-                    err
-                )
-            })?;
+        let response = self.get().await.map_err(|err| {
+            format!(
+                "Couldn't execute Head request! url: {:?}, error: {:#?}",
+                self.url.as_str(),
+                err
+            )
+        })?;
         match response.content_length() {
             Some(val) => self.content_length = val,
             None => {
@@ -212,7 +202,7 @@ impl HttpDownloadConfig {
             header::USER_AGENT,
             HeaderValue::from_str(DEFAULT_USER_AGENT).unwrap(),
         );
-        return config;
+        config
     }
 }
 
@@ -231,7 +221,8 @@ pub async fn quick_download(url: &str) -> Result<(), String> {
         return Err(String::from("Couldn't get UserDirs from OS"));
     }
 
-    let mut download = HttpDownload::new(url, fpath, None).await?;
+    let client = Client::new();
+    let mut download = HttpDownload::new(url, fpath, client, None).await?;
     download.start().await
 }
 
@@ -240,10 +231,12 @@ mod test {
     use super::*;
     use futures::FutureExt;
     use pretty_assertions::assert_eq;
-    use tokio::fs::remove_file;
     use std::error::Error;
     use std::ptr::addr_of_mut;
+    use std::sync::Arc;
     use tempfile::TempDir;
+    use tokio::fs::remove_file;
+    use tokio::sync::Mutex;
 
     async fn setup_test_download() -> Result<(HttpDownload, TempDir), Box<dyn Error>> {
         let tmp_dir = TempDir::new()?;
@@ -251,8 +244,47 @@ mod test {
         let url_str = "https://github.com/yourkin/fileupload-fastapi/raw/a85a697cab2f887780b3278059a0dd52847d80f3/tests/data/test-10mb.bin";
         let url = Url::parse(url_str)?;
         let file_path = tmp_path.join(PathBuf::from(parse_filename(&url).unwrap()));
-        let download = HttpDownload::new(url, file_path, None).await?;
+        let download = HttpDownload::new(url, file_path, Client::new(), None).await?;
         Ok((download, tmp_dir))
+    }
+
+    #[tokio::test]
+    async fn premium_ddownload_test() -> Result<(), Box<dyn Error>> {
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn concurrent_download_test() -> Result<(), Box<dyn Error>> {
+        let mut download_arcs = Vec::new();
+        let mut futures = Vec::new();
+        let mut _tmp_dir_owner = Vec::new();
+        for _ in 0..60 {
+            let (download, _tmp_dir) = setup_test_download().await.unwrap();
+            _tmp_dir_owner.push(_tmp_dir);
+            let download_arc = Arc::new(Mutex::new(download));
+            download_arcs.push(download_arc.clone());
+            let task = tokio::task::spawn(async move {
+                let mut guard = download_arc.lock().await;
+                guard.start().await.unwrap();
+            });
+            futures.push(task);
+        }
+        futures::future::join_all(futures).await;
+
+        for download_arc in download_arcs {
+            let download = download_arc.lock().await;
+            assert_eq!(
+                download.content_length,
+                file_size(&download.file_path).await,
+                "File size should be equal to content_length"
+            );
+            assert_eq!(
+            download.downloaded_bytes,
+            download.content_length,
+            "The downloaded bytes need to be equal to the content_length when the download is finished"
+        );
+        }
+        Ok(())
     }
 
     #[tokio::test]
@@ -262,7 +294,7 @@ mod test {
         let url = Url::parse(url_str)?;
         let file_path = PathBuf::from(parse_filename(&url).unwrap());
         // when creating a download, server data is present in the download struct
-        let download = HttpDownload::new(url, file_path, None).await?;
+        let download = HttpDownload::new(url, file_path, Client::new(), None).await?;
         // then
         assert!(
             download.supports_byte_ranges,
@@ -272,6 +304,7 @@ mod test {
             download.content_length, 10485760000,
             "content-length should be exactly the same as always for the 10GB file"
         );
+        std::mem::drop(download);
         Ok(())
     }
 
