62a8b07605e5324b88221e6d4386b4bcbe64a3bb
lurian <lurian-code@protonmail.com>
2023-03-26 12:49:51 +0200

    Download is now run in event loop correctly, Sender/Stopper works

--- download/src/api.rs
+++ download/src/api.rs
@@ -1,12 +1,13 @@
 use async_trait::async_trait;
 use reqwest::Url;
 use thiserror::Error;
+use tokio::sync::oneshot::Sender;
+use tokio::task::JoinHandle;
 
 #[async_trait]
 pub trait Download {
-    async fn start(&self) -> Result<u64>;
-    async fn stop(&self) -> Result<()>;
-    async fn resume(&self) -> Result<u64>;
+    async fn start(&self) -> Result<(JoinHandle<Result<u64>>, Sender<()>)>;
+    async fn resume(&self) -> Result<(JoinHandle<Result<u64>>, Sender<()>)>;
 }
 
 #[async_trait]
@@ -22,6 +23,12 @@ pub enum Error {
     Request(#[from] reqwest::Error),
     #[error("Content length not provided for url: '{0}'")]
     MissingContentLength(Url),
+    #[error("Failed sending stop signal to download: '{0}'")]
+    StopFailure(Url),
+    #[error("Prematurely dropped channel for download with url: '{0}', downloaded bytes before drop: '{1}'")]
+    ChannelDrop(u64, Url),
+    #[error("Download was already finished, downloaded bytes: '{0}'")]
+    DownloadComplete(u64),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
--- download/src/httpdownload.rs
+++ download/src/httpdownload.rs
@@ -15,8 +15,10 @@ use thiserror;
 use thiserror::Error;
 use tokio::fs::{File, OpenOptions};
 use tokio::io::AsyncWriteExt;
+use tokio::sync::oneshot::error::TryRecvError;
 use tokio::sync::oneshot::{channel, Receiver, Sender};
 use tokio::sync::{oneshot, Mutex};
+use tokio::task::JoinHandle;
 
 use crate::api::{Download, Error, Result};
 use crate::util::{file_size, parse_filename, supports_byte_ranges};
@@ -45,14 +47,12 @@ pub struct HttpDownload {
     * This value gets updated by the struct
      */
     supports_byte_ranges: bool,
-    download_tx: Sender<()>,
-    download_rx: Receiver<()>,
 }
 
 #[async_trait]
 impl Download for HttpDownload {
     /** Starts the Download from scratch */
-    async fn start(&self) -> Result<u64> {
+    async fn start(&self) -> Result<(JoinHandle<Result<u64>>, Sender<()>)> {
         let resp = self
             .client
             .get(self.url.as_ref())
@@ -60,23 +60,29 @@ impl Download for HttpDownload {
             .headers(self.config.headers.clone())
             .send()
             .await?;
+        let (tx, rx) = channel::<()>();
         let file_handler = File::create(&self.file_path).await?;
-        Ok(self.download(resp, file_handler).await?)
-    }
-    fn stop(&self) -> Result<()> {
-        self.download_tx.send(()).unwrap();
-        Ok(())
+        let f = HttpDownload::progress(
+            self.url.clone(),
+            self.config.chunk_size,
+            resp,
+            file_handler,
+            rx,
+        );
+        let handle: JoinHandle<Result<u64>> = tokio::spawn(f);
+        Ok((handle, tx))
     }
 
-    async fn resume(&self) -> Result<u64> {
+    async fn resume(&self) -> Result<(JoinHandle<Result<u64>>, Sender<()>)> {
         let downloaded_bytes = self.get_bytes_on_disk().await;
         if downloaded_bytes == self.content_length {
             log::warn!(
                 "Tried downloading a file that was already downloaded: {}",
                 self.url
             );
-            return Ok(downloaded_bytes);
+            return Err(Error::DownloadComplete(downloaded_bytes));
         }
+        let (tx, rx) = channel::<()>();
         if !self.supports_byte_ranges {
             log::warn!(
                 "Tried resuming a download that doesn't support byte ranges: {}",
@@ -99,7 +105,16 @@ impl Download for HttpDownload {
             .header(RANGE, format!("bytes={}-", downloaded_bytes))
             .send()
             .await?;
-        Ok(downloaded_bytes + self.download(resp, file_handler).await?)
+        let chunk_size = self.config.chunk_size;
+        let url = self.url.clone();
+        let f = async move {
+            Ok(
+                HttpDownload::progress(url, chunk_size, resp, file_handler, rx).await?
+                    + downloaded_bytes,
+            )
+        };
+        let handle: JoinHandle<Result<u64>> = tokio::spawn(f);
+        Ok((handle, tx))
     }
 }
 
@@ -116,14 +131,11 @@ impl HttpDownload {
     ) -> Result<Self> {
         // If no configuration is passed the default one is copied
         let config = config.unwrap_or_default();
-        let (download_tx, download_rx): (Sender<()>, Receiver<()>) = channel();
         let mut download = HttpDownload {
             url,
             file_path,
             config,
             client,
-            download_rx,
-            download_tx,
             supports_byte_ranges: false,
             content_length: 0u64,
         };
@@ -131,14 +143,32 @@ impl HttpDownload {
         Ok(download)
     }
 
-    async fn download(&self, resp: Response, mut file_handler: File) -> Result<u64> {
+    async fn progress(
+        url: Url,
+        chunk_size: usize,
+        resp: Response,
+        mut file_handler: File,
+        mut stopper: Receiver<()>,
+    ) -> Result<u64> {
         let mut downloaded_bytes = 0u64;
         // Await the response, raise error with String msg otherwise
-        let mut stream = resp.bytes_stream().chunks(self.config.chunk_size);
+        let mut stream = resp.bytes_stream().chunks(chunk_size);
         while let Some(buffered_chunks) = stream.next().await {
             for item in buffered_chunks {
                 let chunk = item?;
                 downloaded_bytes += file_handler.write(&chunk).await? as u64;
+            }
+            match stopper.try_recv() {
+                Ok(_) => {
+                    log::info!("Download stop signal received for: {}", url);
+                    return Ok(downloaded_bytes);
+                }
+                Err(TryRecvError::Empty) => {}
+                Err(TryRecvError::Closed) => {
+                    log::error!("Download stop signal channel closed for: {}", url);
+                    log::info!("Stopping download because of error: {}", url);
+                    return Err(Error::ChannelDrop(downloaded_bytes, url.clone()));
+                }
             }
         }
         Ok(downloaded_bytes)
@@ -197,31 +227,30 @@ mod test {
 
     #[tokio::test]
     async fn concurrent_download_test() -> Test<()> {
-        let mut download_arcs = Vec::new();
-        let mut futures = Vec::new();
+        let mut handles = Vec::new();
+        let mut downloads = Vec::new();
+        // Needed because if the tmp dir is dropped it is actually deleted in the Drop impl
         let mut _tmp_dir_owner = Vec::new();
+        let mut _stopper_owner = Vec::new();
         for _ in 0..60 {
             let (download, _tmp_dir) = setup_test_download().await.unwrap();
             _tmp_dir_owner.push(_tmp_dir);
-            let download_arc = Arc::new(Mutex::new(download));
-            download_arcs.push(download_arc.clone());
-            let task = tokio::task::spawn(async move {
-                let download_guard = download_arc.lock().await;
-                download_guard.start().await.unwrap()
-            });
-            futures.push(task);
+            let (handle, _stopper) = download.start().await?;
+            _stopper_owner.push(_stopper);
+            downloads.push(download);
+            handles.push(handle);
         }
-        let results = futures::future::join_all(futures).await;
-        for (download_arc, downloaded_bytes) in zip(download_arcs, results) {
-            let download_guard = download_arc.lock().await;
+        let results = futures::future::join_all(handles).await;
+        for (download, result) in zip(downloads, results) {
+            let downloaded_bytes = result??;
             assert_eq!(
-                download_guard.content_length,
-                file_size(&download_guard.file_path).await,
+                download.content_length,
+                download.get_bytes_on_disk().await,
                 "File size should be equal to content_length"
             );
             assert_eq!(
-                downloaded_bytes?,
-                download_guard.content_length,
+                downloaded_bytes,
+                download.content_length,
                 "The downloaded bytes need to be equal to the content_length when the download is finished"
             );
         }
@@ -253,7 +282,9 @@ mod test {
         // given
         let (download, _tmp_dir) = setup_test_download().await?;
         // when
-        let downloaded_bytes = download.start().await?;
+        let (download_handle, _stopper) = download.start().await?;
+        let join_result = download_handle.await;
+        let downloaded_bytes = join_result??;
         // then
         assert_eq!(
             download.content_length,
@@ -277,7 +308,8 @@ mod test {
         let (mut download, _tmp_dir) = setup_test_download().await?;
         download.config = config;
         // when
-        let downloaded_bytes = download.start().await?;
+        let (download_handle, _stopper) = download.start().await?;
+        let downloaded_bytes = download_handle.await??;
         // then
         assert_eq!(
             download.content_length,
