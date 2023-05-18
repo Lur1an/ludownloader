a1bd5454d472f5a9598adaa82d38866ee99e8a72
lurian <lurian-code@protonmail.com>
2023-05-18 19:02:39 +0200

    Add new error variant for download not existing and update `start` method to use update channel when starting download in Download Manager

--- download-manager/src/lib.rs
+++ download-manager/src/lib.rs
@@ -7,7 +7,10 @@ use tokio::{sync::mpsc, task::JoinHandle};
 use uuid::Uuid;
 
 #[derive(Debug, Error)]
-pub enum Error {}
+pub enum Error {
+    #[error("Download does not exist")]
+    DownloadDoesNotExist,
+}
 
 type Result<T> = std::result::Result<T, Error>;
 
@@ -45,15 +48,13 @@ impl DownloaderItem {
         self.handle.is_some()
     }
 
-    fn start(&mut self) {
+    fn start(&mut self, update_ch: mpsc::Sender<DownloadUpdate>) {
         match &self.download {
             Download::HttpDownload(download) => {
                 let (tx, rx) = tokio::sync::oneshot::channel();
                 let download_arc = download.clone();
-                let (update_sender, _) = mpsc::channel(1000);
 
-                let handle =
-                    tokio::spawn(async move { download_arc.start(rx, update_sender).await });
+                let handle = tokio::spawn(async move { download_arc.start(rx, update_ch).await });
                 self.handle = Some((handle, tx));
             }
         }
@@ -62,33 +63,65 @@ impl DownloaderItem {
 
 #[derive(Debug)]
 struct DownloadManager {
-    update_sender: mpsc::Sender<DownloadUpdate>,
+    update_ch: mpsc::Sender<DownloadUpdate>,
+    consumer_thread: JoinHandle<()>,
     items: HashMap<Uuid, DownloaderItem>,
 }
 
 #[async_trait]
 trait UpdateConsumer {
-    async fn consume(&mut self, update: DownloadUpdate);
+    async fn consume(&self, update: DownloadUpdate);
+}
+
+#[derive(Default)]
+struct DefaultUpdateConsumer {}
+
+#[async_trait]
+impl UpdateConsumer for DefaultUpdateConsumer {
+    async fn consume(&self, update: DownloadUpdate) {
+        println!("Update: {:?}", update);
+    }
+}
+
+impl Default for DownloadManager {
+    fn default() -> Self {
+        let updater = DefaultUpdateConsumer::default();
+        DownloadManager::new(updater)
+    }
 }
 
 impl DownloadManager {
-    fn new(update_sender: mpsc::Sender<DownloadUpdate>) -> Self {
-        let (update_recv, update_send) = mpsc::channel::<DownloadUpdate>(1000);
+    fn new(update_consumer: impl UpdateConsumer + Send + Sync + 'static) -> Self {
+        let (update_sender, mut update_recv) = mpsc::channel::<DownloadUpdate>(1000);
+        log::info!("Spawning update consumer task");
+        let consumer_thread = tokio::task::spawn(async move {
+            while let Some(update) = update_recv.recv().await {
+                update_consumer.consume(update).await;
+            }
+            log::info!("Update channel closed, last update_sender has been dropped");
+        });
+
         DownloadManager {
-            update_sender,
+            consumer_thread,
+            update_ch: update_sender,
             items: HashMap::new(),
         }
     }
 
-    fn add(&mut self, download: Download) {
+    fn add(&mut self, download: Download) -> Uuid {
         let item = DownloaderItem::new(download);
-        self.items.insert(item.download.id(), item);
+        let id = item.download.id();
+        self.items.insert(id, item);
+        id
     }
 
     fn start(&mut self, id: Uuid) -> Result<()> {
-        let item = self.items.get_mut(&id).unwrap();
-        item.start();
-        Ok(())
+        if let Some(item) = self.items.get_mut(&id) {
+            item.start(self.update_ch.clone());
+            Ok(())
+        } else {
+            Err(Error::DownloadDoesNotExist)
+        }
     }
 }
 
@@ -103,8 +136,7 @@ mod test {
 
     #[tokio::test]
     async fn add_download() -> Test<()> {
-        let (update_sender, _) = mpsc::channel::<DownloadUpdate>(1000);
-        let mut manager = DownloadManager::new(update_sender);
+        let mut manager = DownloadManager::default();
         Ok(())
     }
 }
