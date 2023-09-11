f016054a749b595c338b1f463b2cac75b2d25305
lurian <lurian-code@protonmail.com>
2023-09-11 07:24:42 +0200

    WIP: does not build, moving structs to openapi spec and refactoring http routes

--- backend/server/src/api/mod.rs
+++ backend/server/src/api/mod.rs
@@ -1,10 +1,1 @@
-use downloader::httpdownload::{download, DownloadMetadata};
-use serde::{Deserialize, Serialize};
-
 include!(concat!(env!("OUT_DIR"), "/openapi.rs"));
-
-#[derive(Serialize, Deserialize)]
-pub struct DownloadData {
-    pub metadata: DownloadMetadata,
-    pub state: download::State,
-}
