b1a583d57079b457df44be3e120cc4f2d81e8e81
lur1an <lurian-code@protonmail.com>
2024-06-29 12:00:55 +0200

    experimenting with .torrent

--- README.md
+++ README.md
@@ -11,5 +11,6 @@ A download manager with Rust backend with a simple and open API to allow multipl
 - [ ] A way to manage multiple clients (different interfaces/proxies/settings) for Downloads
 - [ ] Premium download hoster implementations (e.g. rapidgator, uploaded, ...) on top of the HttpDownload module
 - [ ] Managing credentials
-- [ ] Module p2pdownload
+- [ ] Module libtorrent
 - [ ] Module Hyperdownload (Downloading multiple download-parts in parallel with different proxies to bypass server speed-limits)
+- [ ] Leptos Frontend
--- lubit/src/lib.rs
+++ lubit/src/lib.rs
@@ -1,14 +1,74 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use compact_str::CompactString;
+use serde::{Deserialize, Serialize};
+use serde_bencode::de;
+use serde_bytes::ByteBuf;
+use std::io::{self, Read};
+
+#[derive(Debug, Deserialize)]
+struct Node(String, i64);
+
+#[derive(Debug, Deserialize)]
+struct File {
+    pub path: Vec<CompactString>,
+    pub length: i64,
+    #[serde(default)]
+    pub md5sum: Option<CompactString>,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct Info {
+    pub name: CompactString,
+    pub pieces: ByteBuf,
+    #[serde(rename = "piece length")]
+    pub piece_length: i64,
+    #[serde(default)]
+    pub md5sum: Option<CompactString>,
+    #[serde(default)]
+    pub length: Option<i64>,
+    #[serde(default)]
+    pub files: Option<Vec<File>>,
+    #[serde(default)]
+    pub private: Option<u8>,
+    #[serde(default)]
+    pub path: Option<Vec<CompactString>>,
+    #[serde(default)]
+    #[serde(rename = "root hash")]
+    pub root_hash: Option<CompactString>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Torrent {
+    pub info: Info,
+    #[serde(default)]
+    pub announce: Option<CompactString>,
+    #[serde(default)]
+    pub nodes: Option<Vec<Node>>,
+    #[serde(default)]
+    pub encoding: Option<CompactString>,
+    #[serde(default)]
+    pub httpseeds: Option<Vec<CompactString>>,
+    #[serde(default)]
+    #[serde(rename = "announce-list")]
+    pub announce_list: Option<Vec<Vec<CompactString>>>,
+    #[serde(default)]
+    #[serde(rename = "creation date")]
+    pub creation_date: Option<i64>,
+    #[serde(rename = "comment")]
+    pub comment: Option<CompactString>,
+    #[serde(default)]
+    #[serde(rename = "created by")]
+    pub created_by: Option<CompactString>,
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn test_fetch_torrent() {
+        let torrent_file = include_bytes!("../../resources/debian.torrent");
+        let parsed_torrent = de::from_bytes::<Torrent>(torrent_file).unwrap();
+        println!("{:?}", parsed_torrent);
     }
 }
